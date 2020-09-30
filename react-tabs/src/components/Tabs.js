31089a0132dc25fa07d5d1616fe607d181c862d2
Aine Ekoh <aineekoh@Aines-MBP.attlocal.net>
2020-09-30 11:23:05 -0500

    add state and active tab

--- react-tabs/src/components/Tabs.js
+++ react-tabs/src/components/Tabs.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
 
 class Tabs extends Component {
+
+    //Setting state and displaying Active Tab
+    static propTypes = {
+        //Array of Tabs
+        children: PropTypes.instanceOf(Array).isRequired,
+      }
+      constructor(props) {
+        super(props);
+    
+        this.state = {
+            //start from beginning of tab array, 0
+          activeTab: this.props.children[0].props.label,
+        };
+      }
+      onClickTabItem = (tab) => {
+        this.setState({ activeTab: tab });
+      }
 
     render() {
         return (
