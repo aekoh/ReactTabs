ce4167ed800bd94367243942187d20cd02883b7e
Aine Ekoh <aineekoh@Aines-MBP.attlocal.net>
2020-09-30 11:01:01 -0500

    created tab components

---
+++ react-tabs/src/components/Tabs.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Tab from './Tab';
+
+class Tabs extends Component {
+
+    render() {
+        return (
+          <div className="tabs">
+                <Tab/>
+          </div>
+        );
+      }
+    }
+    
+    export default Tabs;
