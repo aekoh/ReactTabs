c5c3ebe7db5c304ccd4b7710f8b507ede36ae954
Aine Ekoh <aineekoh@Aines-MBP.attlocal.net>
2020-09-30 14:39:00 -0500

    set state, active state, and keys

--- react-tabs/src/App.js
+++ react-tabs/src/App.js
@@ -2,15 +2,38 @@ import React from 'react';
 import './App.css';
 import Tabs from "./components/Tabs";
 
-function App() {
+class App extends React.Component {
+  // Set state/active
+  constructor(props) {
+    super(props);
+  // default to tabOne
+    this.state = {active: 'tabOne'};
+  }
+  render() {
+    // set id for const's determining which ever tab is active
+    const content = {
+      tabOne: 'Tab 1',
+      tabTwo: 'Tab 2',
+      tabThree: 'Tab 3',
+    };
   return (
     <div className="App">
     <h1>React Tabs</h1>
-    <Tabs>
-      
+    <Tabs
+     active={this.state.active}
+     // changes to selected tab when clicked
+     // setting keys for tabs
+     onChange={active => this.setState({active})}
+    > 
+      <div key="tabOne">Tab 1</div>
+      <div key="tabTwo">Tab 2</div>
+      <div key="tabThree">Tab 3</div>
     </Tabs>
+    <h2>Content</h2>
+    <p>{content[this.state.active]}</p>
     </div>
   );
 }
+};
 
 export default App;
--- react-tabs/src/components/Tabs.js
+++ react-tabs/src/components/Tabs.js
@@ -1,31 +1,17 @@
 import React, { Component } from 'react';
-import Tab from './Tab';
-import PropTypes from 'prop-types';
+import './Tabs.css';
 
 class Tabs extends Component {
-
-    //Setting state and displaying Active Tab
-    static propTypes = {
-        //Array of Tabs
-        children: PropTypes.instanceOf(Array).isRequired,
-      }
-      constructor(props) {
-        super(props);
-    
-        this.state = {
-            //start from beginning of tab array, 0
-          activeTab: this.props.children[0].props.label,
-        };
-      }
-      onClickTabItem = (tab) => {
-        this.setState({ activeTab: tab });
-      }
-
+    componentDidMount() {
+    }
     render() {
         return (
           <div className="tabs">
-                <Tab/>
-          </div>
+                Tabs things
+             <div
+              className="Tabs__select"
+             />
+            </div>
         );
       }
     }
