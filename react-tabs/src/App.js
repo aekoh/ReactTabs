e2e0a975d223829ca42be15e5f37dc906e01e195
Aine Ekoh <aineekoh@Aines-MacBook-Pro.local>
2020-10-05 14:14:23 -0500

    render buttons and content

--- react-tabs/src/App.js
+++ react-tabs/src/App.js
@@ -2,16 +2,48 @@ import React from 'react';
 import './App.css';
 import Tabs from "./components/Tabs";
 
-class App extends React.Component {
-
-  render() {
-    // set id for const's determining which ever tab is active
-  return (
-    <div className="App">
-    <Tabs/>
-    </div>
-  );
-}
-};
-
+  class App extends React.Component {
+    render(){
+      return(
+        <div className="tabsLayout">
+          <h1>Front-end Frameworks</h1>
+         <Tabs>
+           <li label="React">
+             <div>
+               <p>
+              React React is developed and maintained by Facebook and used in their products,
+              including Instagram and WhatsApp. It has been around for around since 2013, so 
+              it’s not exactly new. It’s also one of the most popular projects on GitHub, with
+              more than 150,000 stars at the time of writing. Some of the other notable companies
+              using React are Airbnb, Uber, Netflix, Dropbox, and Atlassian. Sounds good to me.
+                 
+               </p>
+             </div>
+           </li>
+           <li label="Angular">
+             <div>
+             
+               <p>Angular has been around since 2016, making it slightly younger than React,
+                  but it’s also not a new kid on the block. It’s maintained by Google. In 2018 
+                  was used in more than 600 hundred applications in Google such as Firebase Console,
+                  Google Analytics, Google Express, Google Cloud Platform and more. Outside of Google,
+                  Angular is used by Forbes, Upwork, VMWare, and others.
+               </p>
+             </div>
+           </li>
+           <li label="Both">
+             <div>
+               <p>
+                 THEY ARE BOTH GREAT!
+               </p>
+             </div>
+           </li>
+         </Tabs>
+        </div>
+        
+      )
+    }
+  }
+  
+  
 export default App;
--- react-tabs/src/components/Tabs.js
+++ react-tabs/src/components/Tabs.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
 import './Tabs.css';
 
-function Tabs() {
-    const Tabs = ['Cowboys','Patriots'];
-        return (
-            <div className="App">
-            <h1>React Tabs</h1>
-            </div>
-        );
-      }
+class Tabs extends React.Component{
+    // Set state for active tab that will display everything related to each tab 
+    state ={
+      activeTab: this.props.children[0].props.label
+    }
+    changeTab = (tab) => {
+  
+      this.setState({ activeTab: tab });
+    };
+    render(){
+      
+      let content;
+      let buttons = [];
+      return (
+        <div>
+          {React.Children.map(this.props.children, child =>{
+            buttons.push(child.props.label)
+            // If label is equal to 
+            if (child.props.label === this.state.activeTab) content = child.props.children
+          })}
+           
+          <TabButtons activeTab={this.state.activeTab} buttons={buttons} changeTab={this.changeTab}/>
+          <div className="tab-content">{content}</div>
+          
+        </div>
+      );
+    }
+  }
+
+  const TabButtons = ({buttons, changeTab, activeTab}) =>{
+     
+    return(
+      <div className="tab-links">
+      {buttons.map(button =>{
+         return <button className={button === activeTab? 'active': ''} onClick={()=>changeTab(button)}>{button}</button>
+      })}
+      </div>
+    )
+  }
+  
+  const Tab = props =>{
+    return(
+      <React.Fragment>
+        {props.children}
+      </React.Fragment>
+    )
+  }
+
     
     export default Tabs;
