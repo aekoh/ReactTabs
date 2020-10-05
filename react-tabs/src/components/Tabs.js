c532b3da0d35f169084f5c7ddad24b5906104c2a
Aine Ekoh <aineekoh@Aines-MacBook-Pro.local>
2020-10-05 10:14:27 -0500

    buttons

--- react-tabs/src/components/Tabs.js
+++ react-tabs/src/components/Tabs.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import './Tabs.css';
 
-class Tabs extends Component {
-    componentDidMount() {
-    }
-    render() {
+function Tabs() {
+    const Tabs = ['Cowboys','Patriots'];
         return (
             <div className="App">
             <h1>React Tabs</h1>
             </div>
         );
       }
-    }
     
     export default Tabs;
